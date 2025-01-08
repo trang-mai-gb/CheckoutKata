412aebd9ea5f45f9b3ac1ed251e53a06433f175d
Trang Mai <trang.mai.gb@gmail.com>
2025-01-08 04:49:06 +0000

    Add IPricing interface

---
+++ CheckoutProject/CheckoutProject/IPricing.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckoutProject
+{
+    public interface IPricing
+    {
+        public int CalculatePrice(string item, PricingRule pricingRule);
+    }
+}
---
+++ CheckoutProject/CheckoutProject/Pricing.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckoutProject
+{
+    public class Pricing : IPricing
+    {
+        public int CalculatePrice(int quantity, PricingRule rule)
+        {
+            if (pricingRule.SpecialPrice.HasValue && pricingRule.SpecialPriceQuantity.HasValue)
+            {
+                totalPrice += (int)((item.Value / pricingRule.SpecialPriceQuantity) * pricingRule.SpecialPrice + (item.Value % pricingRule.SpecialPriceQuantity) * pricingRule.Price);
+            }
+            else
+            {
+                totalPrice += item.Value * pricingRule.Price;
+            }
+
+        }
+    }
+}
