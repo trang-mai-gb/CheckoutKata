5b92ebb720614f8e1c692f51af73164e495af5ae
Trang Mai <trang.mai.gb@gmail.com>
2025-01-08 04:11:48 +0000

    Add reference project

---
+++ CheckoutProject/CheckoutTestProject/CheckoutTests.cs
@@ -0,0 +1,16 @@
+using CheckoutProject;
+namespace CheckoutTestProject
+{
+    public class CheckoutTests
+    {
+        private List<PricingRule>
+
+
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
